af5f4c3ffc76f58d23d65aac5fdb44c1a148e571
Zeolow <zeo.loe@gmail.com>
2023-05-28 18:48:14 +0200

    resize bug fix

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+
 function toggle_menu(){
   var toggleButton = document.getElementById("menu-toggle")
-  var mainText = document.getElementById("main-text")
   var menu = document.getElementById("menu-list")
   var toggleButtonInside = document.getElementById("menu-toggle-inside")
+  
 
   if (menu.style.display === "none" || menu.style.display === ""){
-    console.log(toggleButton.display)
     menu.style.display = "block"
-    console.log(toggleButton)
     toggleButton.style.display= "none"
-    console.log(toggleButtonInside)
     toggleButtonInside.style.display= "block"
   } else {
     menu.style.display = "none"
     toggleButton.style.display="block"
     toggleButtonInside.style.display= "none"
+  }
+}
+
+function on_resize(){
+  var toggleButton = document.getElementById("menu-toggle")
+  var menu = document.getElementById("menu-list")
+  var toggleButtonInside = document.getElementById("menu-toggle-inside")
+  
+  if(window.innerWidth >= 1000) {
+    menu.style.display="block"
+    toggleButton.style.display="none"
+    toggleButtonInside.style.display= "none"
+  } else {
+    toggleButtonInside.style.display= "block"
   }
 }
