3e58bc84d9615ceef0988409ad61c1644ca7791d
Zeo <zeo.loe@gmail.com>
2023-05-28 14:55:31 +0200

    mobile bar

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggle_menu(){
+  var toggleButton = document.getElementById("menu-toggle")
+  var mainText = document.getElementById("main-text")
+  var menu = document.getElementById("menu-list")
+
+  if (menu.style.display === "none"){
+    console.log(toggleButton.display)
+    menu.style.display = "block"
+    mainText.display = "none"
+    toggleButton.display= "none"
+    toggleButton.innerText="(⇦)"
+  } else {
+    menu.style.display = "none"
+    mainText.display = "block"
+    toggleButton.display="block"
+    toggleButton.innerText="(⇨)"
+  }
+}
